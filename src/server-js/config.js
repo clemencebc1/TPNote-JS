fb4bbbb7705687f23a5492eeeba33ed68d4a9d30
Clemence <clemence.bcq@gmail.com~>
2025-03-04 10:49:24 +0100

    construction arborescence routes

--- src/server-js/config.js
+++ src/server-js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_ENDPOINT: "http://localhost:3000" 
+    API_ENDPOINT: "http://localhost:8000" 
 };
 
 export default CONFIG;
---
+++ src/views/Home.js
@@ -0,0 +1,6 @@
+
+export default class Home {
+     async render(){
+        return `<h1>Home Page</h1>`;
+     }
+}
